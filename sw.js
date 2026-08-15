9dab8aa51ce49960f13f0c2b3a64d6a764bae135
Giliard31 <113076201+Giliard31@users.noreply.github.com>
2026-08-14 22:49:18 -0300

    Change cached file from .json to .txt

--- sw.js
+++ sw.js
@@ -3,26 +3,19 @@ const urlsToCache = [
     './',
     './index.html',
     './manifest.json',
-    './harpa_crista_640_hinos.json' // Garanta que este arquivo esteja na raiz ou ajuste o caminho
+    './harpa_crista_640_hinos.txt' // Alterado para .txt
 ];
 
-// Instala o Service Worker e guarda os arquivos no cache
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Intercepta as requisições para entregar o conteúdo offline
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                // Retorna do cache se existir, caso contrário busca na rede
-                return response || fetch(event.request);
-            })
+            .then(response => response || fetch(event.request))
     );
 });
